6c30cf6572254461c4d2886d37a3bd3ae3f13e60
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-02-15 15:46:54 +0100

    Rotta per la mia Show

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Guest\PageController;
 
 Route::get('/', [PageController::class, 'index']);
+
+//Creo la rotta per la mia Show
+Route::get('/comics/{id}', [PageController::class, 'show']);
